74d8ebce7aec3615d1cdc49c12b042cf1c79cc90
jendiamond <thejendiamond@gmail.com>
2024-05-07 00:59:08 -0700

    add all vite and vue3 files

---
+++ src/components/Logo.vue
@@ -0,0 +1,118 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div class="greetings">
+    <span class="deans-racket">
+      dean's racket
+    </span>
+    <div class="center-cirles"><div class="circles"></div></div>
+  </div>
+</template>
+
+<style scoped>
+
+.greetings {
+
+  background-color: aliceblue;
+
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: center;
+  align-items: center;
+}
+
+/* .dean {
+background-image: linear-gradient(45deg, #f2f0f5 25%, #05010a 25%, #05010a 50%, #f2f0f5 50%, #f2f0f5 75%, #05010a 75%, #05010a 100%);
+background-size: 28.28px 28.28px;
+} */
+
+/* @media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+} */
+
+/* h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+} */
+
+/* .birthday {
+  font-size: 2rem;
+  color:#358cd9;
+  text-align: center;
+} */
+
+.deans-racket {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: center;
+  align-content: stretch;
+  align-items: center;
+
+  z-index: 30;
+  margin-bottom: 10px;
+  padding: 25px;
+  font-size: 5rem;
+  color:#358cd9;
+  color: black;
+}
+
+.center-cirles {
+  position: absolute;
+  z-index: 20;
+
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: center;
+  align-content: stretch;
+  align-items: center;
+}
+.circles,
+.circles:before,
+.circles:after {
+  border-radius: 50%;
+  width: 200px;
+  height: 200px;
+  content: '';
+  display: block;
+}
+
+.circles {
+  background-color: #358cd9;
+  position: relative;
+  top: 20px;
+  left: 10px;
+  bottom: 50px;
+  mix-blend-mode: lighten;
+}
+
+.circles:before,
+.circles:after {
+  position: absolute;
+}
+
+.circles:before {
+  left: -80px;
+  top: 110px;
+  background-color: #ed740b;
+  mix-blend-mode: lighten;
+}
+
+.circles:after {
+  left: 90px;
+  bottom: 125px;
+  background-color: #22e978;
+  mix-blend-mode: overlay;
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,113 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <div class="songs">
+    <WelcomeItem>
+      <template #icon>
+        <SupportIcon />
+      </template>
+      <template #heading>Dean's Fun</template>
+      Lyrics & Video
+    </WelcomeItem>
+
+    <WelcomeItem>
+      <template #icon>
+        <SupportIcon />
+      </template>
+      <template #heading>In Your Body</template>
+      Lyrics in collaboration with Carla Denka
+      Lyrics & Video
+    </WelcomeItem>
+
+    <WelcomeItem>
+      <template #icon>
+        <SupportIcon />
+      </template>
+      <template #heading>Exquisite Man</template>
+      Lyrics in collaboration with Kevin Hanley
+    </WelcomeItem>
+
+    <WelcomeItem>
+      <template #icon>
+        <SupportIcon />
+      </template>
+      <template #heading>I Love your Nose</template>
+      <p>Lyrics in collaboration with Michael Urban</p>
+    </WelcomeItem>
+
+    <WelcomeItem>
+      <template #icon>
+        <SupportIcon />
+      </template>
+      <template lass= "heading-item" #heading>Man Among Men</template>
+
+      As an independent project, Vue relies on community backing for its sustainability. You can help
+      us by
+      <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
+    </WelcomeItem>
+      <RouterView />
+<span class="divider">================</span>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+  </div>
+</template>
+
+<style scoped>
+.songs {
+  background-color:#d935bb;
+}
+
+.heading-item {
+  z-index: 20;
+  color:rgb(240, 255, 255);
+  font-size: 1.5rem;
+}
+
+.divider {
+  font-size: 12px;
+  color: white;
+  display: flex;
+  place-items: center;
+  place-content: center;
+  padding-top: 10px;;
+}
+
+nav {
+  width: 100%;
+  font-size: 16px;
+  text-align: center;
+  margin-top: .5rem;
+  padding-bottom: 10px;
+  z-index: 30;
+}
+
+nav a.router-link-exact-active {
+  color: white;
+  font-weight: 500;
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+  color: black;
+  font-weight: 500;
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+</style>
